9e248f9a4ba652c7462567ffdf8046b1da587fd1
Jason Boeshart <jboeshart@users.noreply.github.com>
2020-02-12 15:41:15 -0600

    Update AddDatabaseToSqlServer.ps1
    Disabling IE Enhanced Security Configuration for administrators via script so we don't have to step through it in the exercise.

--- AddDatabaseToSqlServer.ps1
+++ AddDatabaseToSqlServer.ps1
@@ -6,6 +6,10 @@ param(
     $password
 )
 
+# Disable IE Enhanced Security Configuration so we can download the Data Migration Assistant
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+
 if ((Get-Command Install-PackageProvider -ErrorAction Ignore) -eq $null)
 {
 	# Load the latest SQL PowerShell Provider
