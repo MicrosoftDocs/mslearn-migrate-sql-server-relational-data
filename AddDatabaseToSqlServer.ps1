09612c9457096a76ba4cebccaf285f5f2ee56df5
Jason Boeshart <jboeshart@users.noreply.github.com>
2020-02-13 11:59:38 -0600

    Update AddDatabaseToSqlServer.ps1

--- AddDatabaseToSqlServer.ps1
+++ AddDatabaseToSqlServer.ps1
@@ -6,12 +6,6 @@ param(
     $password
 )
 
-# Disable IE Enhanced Security Configuration so we can download the Data Migration Assistant
-$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
-Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
-    
 if ((Get-Command Install-PackageProvider -ErrorAction Ignore) -eq $null)
 {
 	# Load the latest SQL PowerShell Provider
